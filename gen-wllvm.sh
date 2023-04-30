b03df2efe2bc510b8b75299a673f2639bae6ce7a
Mingjie Shen <shen497@purdue.edu>
2023-04-30 00:14:30 +0000

    update

--- gen-wllvm.sh
+++ gen-wllvm.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-DB=$RTOSExploration/bitcode-db/RP2040-FreeRTOS
-rm -rf $DB build && mkdir -p build
-cmake -S . -B build
-genbc $DB -c "cmake --build build -j$(nproc)"
+#DB=$RTOSExploration/bitcode-db/RP2040-FreeRTOS
+#rm -rf $DB build && mkdir -p build
+#cmake -S . -B build
+#cmake --build build -j$(nproc)
+
+export LLVM_COMPILER=hybrid
+export LLVM_COMPILER_PATH=/usr/lib/llvm-14/bin
+export GCC_CROSS_COMPILE_PREFIX=arm-none-eabi-
+rm -rf build
+#CC=wllvm CXX=wllvm++ cmake -S . -B build
+cmake -S . -B build -DCMAKE_C_COMPILER=$(which wllvm) -DCMAKE_CXX_COMPILER=$(which wllvm++)
+cmake --build build -j$(nproc)
+find build/ -name "*.elf" | xargs -n 1 extract-bc
